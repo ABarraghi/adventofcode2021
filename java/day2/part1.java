3157d250ac6be55e6dd52db4dca9c2356d32f241
Arman Barraghi <abarraghi@gmail.com>
2021-12-02 23:15:40 +0800

    Day 2 solved with Java

---
+++ java/day2/part1.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+
+public class part1 {
+
+    public static void main(String[] args) {
+
+        try {
+            int depth = 0, forward = 0;
+            Scanner reader = new Scanner(new File("input.txt"));
+
+            while (reader.hasNextLine()) {
+
+                String[] currCommand = reader.nextLine().split(" ");
+                switch (currCommand[0]) {
+                    case "forward":
+                        forward += Integer.parseInt(currCommand[1]);
+                        break;
+                    case "down":
+                        depth += Integer.parseInt(currCommand[1]);
+                        break;
+                    case "up":
+                        depth -= Integer.parseInt(currCommand[1]);
+                        break;
+                }
+            }
+
+            System.out.println("Total depth: " + depth);
+            System.out.println("Total forward: " + forward);
+            System.out.println("Product: " + (depth * forward));
+        } catch (Exception e) {
+        }
+
+    }
+
+}
---
+++ java/day2/part2.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+
+public class part2 {
+
+    public static void main(String[] args) {
+
+        try {
+            int aim = 0, forward = 0, depth = 0;
+            Scanner reader = new Scanner(new File("input.txt"));
+
+            while (reader.hasNextLine()) {
+
+                String[] currCommand = reader.nextLine().split(" ");
+                switch (currCommand[0]) {
+                    case "forward":
+                        forward += Integer.parseInt(currCommand[1]);
+                        depth += Integer.parseInt(currCommand[1]) * aim;
+                        break;
+                    case "down":
+                        aim += Integer.parseInt(currCommand[1]);
+                        break;
+                    case "up":
+                        aim -= Integer.parseInt(currCommand[1]);
+                        break;
+                }
+            }
+
+            System.out.println("Total aim: " + aim);
+            System.out.println("Total depth: " + depth);
+            System.out.println("Total forward: " + forward);
+            System.out.println("Product: " + (depth * forward));
+        } catch (Exception e) {
+        }
+
+    }
+
+}
